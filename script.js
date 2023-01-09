b6ad20ba395f091866ee120a3e3e0a1f26d42a56
Bapi Biswas <star.bappy@gmail.com>
2023-01-09 18:22:44 +0600

    homework done

---
+++ script.js
@@ -0,0 +1,59 @@
+// 1. Get Element ID***
+// const id1 = document.getElementById("id1");
+// console.log(id1);
+
+// console.log(id1.innerHTML);
+// console.log(id1.innerText);
+
+// id1.innerHTML = "Java script text";
+
+// document.getElementById("id1").innerHTML = "New Java script text";
+
+// 2. Get element by tag name
+// const tag1 = document.getElementsByTagName("h1")[1];
+// console.log(tag1);
+// console.log(tag1.innerText);
+// tag1.innerText = "Java script Text";
+
+// 3.Get element by class name
+// const class1 = document.getElementsByClassName("class1")[0];
+// console.log(class1);
+
+// class1.innerHTML = "Javascript text";
+
+// DOM element style
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+// id1.style.color = "red";
+// id1.style.backgroundColor = "green";
+// id1.style.fontSize = "50px";
+
+// 5. DOM element attributes
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+
+// console.log(id1.attributes);
+// console.log(id1.attributes.length);
+
+// 6. Get attribute
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+
+// id1.getAttribute("class");
+// console.log(id1.getAttribute("class"));
+// console.log(id1.getAttribute("id"));
+
+// 7. Remove attribute
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+
+// id1.getAttribute("class");
+// id1.removeAttribute("class");
+// console.log(id1.getAttribute("id"));
+
+// setattribute
+// const id2 = document.getElementById("id2");
+// id2.setAttribute("class", "class2");
+
+const tag1 = document.getElementsByTagName("button");
+function textStyle() {}
